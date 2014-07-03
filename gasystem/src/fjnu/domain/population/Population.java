9a7e0a4b1a34f2b42600e3f6fdeaaa59a01c7e7e
youcongni <youcongni@foxmail.com>
2014-07-03 11:30:22 +0800

    Change-Id: Ib7378db41ef3241a10063b86ce5ce156adbd606b Signed-off-by: youcongni <youcongni@foxmail.com>

--- gasystem/src/fjnu/domain/population/Population.java
+++ gasystem/src/fjnu/domain/population/Population.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Random;
 
 import fjnu.domain.chrom.Chromosome;
+import fjnu.domain.input.GACfgInfo;
 import fjnu.domain.input.GAParameter;
 
 /**
@@ -14,14 +15,15 @@ import fjnu.domain.input.GAParameter;
  */
 public class Population {
 
-	private int size;// 种群规模
-	double crossoverProbility;// 交叉概率
-	double mutateProbility;// 交叉概率
+	private int popSize;// 种群规模
+	private double crossoverProbility;// 交叉概率
+	private double mutateProbility;// 交叉概率
+	private GAParameter parameter = null;// 对配置文件的操作，用于获取文件的配置参数；
+	
+	private Random random = null;
+	
+	private List<Chromosome> chromosomes = null;
 
-	Chromosome[] chromosomes = null;
-
-	GAParameter parameter = null;// 对配置文件的操作，用于获取文件的配置参数；
-	Random random = null;
 
 	/**
 	 * 构造器：指定初始化GA参数
@@ -30,20 +32,15 @@ public class Population {
 	 *            parameter
 	 */
 	public Population(GAParameter parameter, Random random) {
-		super();
 		this.random = random;
 		this.parameter = parameter;
-		this.size = parameter.getPopulationSize();
-		this.crossoverProbility = parameter.getCrossoverProbability();
-		this.mutateProbility = parameter.getMutateProbabilty();
-		chromosomes = new Chromosome[size];
-		for (int i = 0; i < size; i++) {
-			chromosomes[i] = new Chromosome(random);
-		}
+		initializePopulation() ;
 	}
 
 	public Population() {
-		super();
+		this.random = random;
+		this.parameter = GACfgInfo.getInstance().getGAParameters();
+		initializePopulation() ;
 	}
 
 	/**
@@ -51,56 +48,29 @@ public class Population {
 	 * 
 	 * @return 一个新的种群
 	 */
-	public Population initializePopulation() {
-		Population newPopulation = new Population();
-		for (int i = 0; i < size; i++) {// 根据种群规模进行初始化个体
-			chromosomes[i] = new Chromosome(random, parameter, null);
+	private void initializePopulation() {
+		this.popSize = parameter.getPopulationSize();
+		this.crossoverProbility = parameter.getCrossoverProbability();
+		this.mutateProbility = parameter.getMutateProbabilty();
+		
+		for (int i = 0; i < popSize; i++) {
+			
+			Chromosome chromosome=new Chromosome();
+			chromosomes.add(chromosome);
 		}
-		newPopulation.setChromosomes(chromosomes);
-		newPopulation.setSize(size);
-		newPopulation.setParameter(parameter);
-		return newPopulation;
 	}
 
 	/**
 	 * 种群的变异操作；变异以后返回中间种群，种群进行变异操作时，是根据 配置文件中的变异概率来决定的
-	 * @throws ClassNotFoundException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
+	 * 
 	 */
-	public void mutate(int mutateNum,List<StringBuffer> codes) {
-		for (int i = 0; i < size; i++) {
+	public void mutate(int mutateNum) {
+		for (int i = 0; i < popSize; i++) {
 			double mutatePro = random.nextDouble();
 			if (mutatePro <= mutateProbility) {
-//				List<StringBuffer> codes1 = chromosomes[i].getEncodes();
-				chromosomes[i].mutate(mutateNum, codes, parameter);
+				chromosomes.get(i).mutate(mutateNum);
 			}
 		}
-
-	}
-
-	public Chromosome[] getChromosomes() {
-		return chromosomes;
-	}
-
-	public void setChromosomes(Chromosome[] chromosomes) {
-		this.chromosomes = chromosomes;
-	}
-
-	public int getSize() {
-		return size;
-	}
-
-	public void setSize(int size) {
-		this.size = size;
-	}
-
-	public GAParameter getParameter() {
-		return parameter;
-	}
-
-	public void setParameter(GAParameter parameter) {
-		this.parameter = parameter;
 	}
 
 }
